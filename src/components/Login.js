0a77ac9cb9d96fcdd65977d673c681efbb172e41
ebennaga <freddy.eben@gmail.com>
2018-01-03 21:26:01 +0700

    switch router

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render(){
+        return (
+            <div className="Login">
+                This is Login
+            </div>
+        );
+    }
+}
+
+
+export default Login;

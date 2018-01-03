817a8ad03914ab9bc814ff316f4be97cead33457
ebennaga <freddy.eben@gmail.com>
2018-01-03 13:43:25 +0700

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    render(){
+        return (
+            <div className="Register">
+                This is Register
+            </div>
+        );
+    }
+}
+
+
+export default Register;
